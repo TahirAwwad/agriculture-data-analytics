e77b04beecd290a98c7d42e3c7bf5d3466995029
swazypat <patryk.nowotka@gmail.com>
2022-01-06 23:13:29 +0000

    cleaning of words not working

--- python-package/agriculture_data_analytics/sentiment_analysis.py
+++ python-package/agriculture_data_analytics/sentiment_analysis.py
@@ -1,6 +1,12 @@
-def preprocess(html_text: str):
-    html_text = html_text.str.replace("(<br/>)", "")
-    html_text = html_text.str.replace('(<a).*(>).*(</a>)', '')
+def preprocess(html_text: str) ->str :
+    """
+    This function takes in a string of html text and returns a string of text
+    with all html tags removed.
+    html_text: string of html text
+    return: string of text with all html tags removed
+    """
+    html_text = html_text.str.replace("(<br/>)","")
+    html_text = html_text.str.replace('(<a).(>).(</a>)', '')
     html_text = html_text.str.replace('(&amp)', '')
     html_text = html_text.str.replace('(&gt)', '')
     html_text = html_text.str.replace('(&lt)', '')
