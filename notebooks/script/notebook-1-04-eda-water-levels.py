4b4a5b95009e6f4445813d70c779c4ff81809c68
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2022-01-11 05:16:06 +0000

    too many null values

--- notebooks/script/notebook-1-04-eda-water-levels.py
+++ notebooks/script/notebook-1-04-eda-water-levels.py
@@ -37,7 +37,7 @@
 # <table style="margin: auto;"><tr><td><a href="https://mybinder.org/v2/gh/markcrowe-com/data-analytics-project-template/master?filepath=notebooks/notebook-1-04-eda-water-levels.ipynb" target="_parent"><img src="https://mybinder.org/badge_logo.svg" alt="Open In Binder"/></a></td><td>online editors</td><td><a href="https://colab.research.google.com/github/markcrowe-com/data-analytics-project-template/blob/master/notebooks/notebook-1-04-eda-water-levels.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a></td></tr></table>
 
 # ### Objective
-# The objective is to provide an Exploratory Data Analysis (EDA) of the water levels data files. The EDA is performed to investigate and clean the data, to spot anomalies.  
+# The objective is to provide an Exploratory Data Analysis (EDA) of the water levels data files. The EDA is performed to investigate and clean the data, to spot anomalies.  Data sourced from [EPA: Water Quality and Monitoring, Groundwater Quality (Excel) 1990 - 2020](https://gis.epa.ie/GetData/Download)
 # ### Setup
 # Import required third party Python libraries, import supporting functions and sets up data source file paths.
 
@@ -51,8 +51,10 @@ from population_planning.dataframe_labels import *
 from population_planning.project_manager import ProjectArtifactManager, ProjectAssetManager
 import data_analytics.github as github
 import data_analytics.exploratory_data_analysis_reports as eda_reports
+import numpy
 import os
 import pandas
+import matplotlib.pyplot as pyplot
 
 
 artifact_manager = ProjectArtifactManager()
@@ -111,7 +113,60 @@ old_to_new_column_names_dictionary = {
 # - Report the count of any duplicate rows.
 # - Report the counts of any missing values.
 
+# A function to vizualize missing data in a DataFrame
+from pandas import DataFrame
+import matplotlib.pyplot as pyplot
+import seaborn 
+
+def calculate_missing_value_stats(dataframe: DataFrame):
+    missing_value_stats_dataframe = DataFrame(
+        {
+            'Missing'   : dataframe.isnull().sum(),
+            '% Missing' : dataframe.isnull().sum() / len(dataframe)
+        })
+    missing_value_stats_dataframe = missing_value_stats_dataframe[missing_value_stats_dataframe['% Missing'] > 0].sort_values(by='Missing', ascending=False)
+    return missing_value_stats_dataframe
+
+def viz_missing(missing_value_stats_dataframe: DataFrame) :
+    pyplot.subplots(figsize=(40, 30))
+    seaborn.barplot(x=missing_value_stats_dataframe.index, y='% Missing', data=missing_value_stats_dataframe)
+    pyplot.xticks(rotation=90)
+    pyplot.show()
+
+
+viz_missing(missing_value_stats_dataframe)
+
+
+missing_value_stats_dataframe = calculate_missing_value_stats(ground_water_dataframe)
+
+missing_value_stats_dataframe
+
+
+dataframe_columns = ground_water_dataframe.columns.values.tolist()
+
+print("Columns:", len(dataframe_columns))
+
+for column in dataframe_columns[3:-1]:
+    try:
+        ground_water_dataframe[column] = ground_water_dataframe[column].astype('int32')
+    except:
+        print(ground_water_dataframe[column].unique())
+
+
 filename = os.path.basename(filepath)
+
+ground_water_dataframe[["Easting", "Northing"]] = ground_water_dataframe[["Easting", "Northing"]].apply(pandas.to_numeric)
+
+
+ground_water_dataframe.replace("-", numpy.nan, inplace = True)
+
+
+ground_water_dataframe.replace("--", numpy.nan, inplace = True)
+
+
+ground_water_dataframe[["Temperature (on-site)"]]= ground_water_dataframe[["Temperature (on-site)"]].apply(pandas.to_numeric)
+
+
 eda_reports.print_dataframe_analysis_report(ground_water_dataframe, filename)
 
 
