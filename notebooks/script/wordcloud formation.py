97c1f477fc8425d7c0ae6a4bcd74c1b848413ac2
swazypat <patryk.nowotka@gmail.com>
2022-01-16 18:17:59 +0000

    wordcloud formation

---
+++ notebooks/script/wordcloud formation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud, ImageColorGenerator
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Loading the text data
+dataframe = pd.read_csv("./../artifacts/agriculture-ie-clean.csv")
+text = str(dataframe['clean_text'])
+print(text)
+# Creating empty WordCloud object and generating actual WordCloud.
+wordcloud = WordCloud().generate(text)
+# Display the word cloud
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+
+
+
+
+
+
