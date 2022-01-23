a688bb06f8a76cc23eba55630679db3115e78dd8
Tahir Awwad <63153794+TahirAwwad@users.noreply.github.com>
2022-01-23 13:41:05 +0000

    Update app1.py

--- notebooks/app1.py
+++ notebooks/app1.py
@@ -9,14 +9,14 @@ st.sidebar.selectbox("Choose Page",("Predction","EDA","Stats"))
 
 
 def load_model():
-    pkl_model = pickle.load(open('tahir-api/notebooks/pkl_ann_milk','rb'))
+    pkl_model = pickle.load(open('pkl_ann_milk','rb'))
     return pkl_model
 
 model = load_model()
 
-pkl_scaler_y = pickle.load(open('tahir-api/notebooks/pkl_scaler_y','rb'))
+pkl_scaler_y = pickle.load(open('pkl_scaler_y','rb'))
  
 
-pkl_scaler_x = pickle.load(open('tahir-api/notebooks/pkl_scaler_x','rb'))
+pkl_scaler_x = pickle.load(open('pkl_scaler_x','rb'))
 
 show_predict_page()
