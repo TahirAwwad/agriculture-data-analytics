963962a7172c47819dc6ae46a93006daa71decf0
Tahir Awwad <63153794+TahirAwwad@users.noreply.github.com>
2022-01-23 15:24:45 +0000

    if function added for page selector

--- artifacts/app1.py
+++ artifacts/app1.py
@@ -2,9 +2,13 @@ import streamlit as st
 import pickle
 import numpy as np
 from predict_page import show_predict_page
+from explore import show_explore_page
 
 
-st.sidebar.selectbox("Choose Page",("Predction","EDA","Stats"))
+page = st.sidebar.selectbox("Choose Page",("Predction","Summary Stats"))
 
-
-show_predict_page()
+if page=='Summary Stats':
+    show_explore_page()
+else:
+    show_predict_page()
+    
---
+++ artifacts/explore.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def show_explore_page():
+    st.title("## Summary Statistics and Visual ")
