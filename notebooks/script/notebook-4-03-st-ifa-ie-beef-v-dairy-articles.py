f22ef2b93e07883a7f39922690771b88fb4fb7e4
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2022-01-27 08:14:16 +0000

    403 ifa beef v dairy

--- notebooks/script/notebook-4-03-st-ifa-ie-beef-v-dairy-articles.py
+++ notebooks/script/notebook-4-03-st-ifa-ie-beef-v-dairy-articles.py
@@ -48,6 +48,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 
 
+from scipy import stats ## Needed for stats test
+import statsmodels.api as sm
+from wordcloud import WordCloud, STOPWORDS
+
+from matplotlib import pyplot as plt
+
+
 get_ipython().run_line_magic('matplotlib', 'inline')
 InteractiveShell.ast_node_interactivity = 'all'
 matplotlib.rcParams['figure.figsize'] = (10.0, 6.0)
@@ -71,6 +78,8 @@ words = set(nltk.corpus.words.words())
 filename: str = "./../assets/ifa-ie-articles.csv"
 df = pd.read_csv(filename)
 
+df.columns 
+
 
 df.info()
 
@@ -81,7 +90,7 @@ def vader_scorer(df):
     Returns: Dataframe of vader scores
     '''
     analyzer = SentimentIntensityAnalyzer()
-    vader_scores = df.loc[:,'text'].map(analyzer.polarity_scores)
+    vader_scores = df.loc[:,'Text'].map(analyzer.polarity_scores)
 
     dvec = DictVectorizer()
     vader_scores = dvec.fit_transform(vader_scores)
@@ -94,23 +103,37 @@ vader_scores = vader_scorer(df)
 df = pd.concat([df,vader_scores], axis=1)
 
 
-beef_dataframe = df[df['Trend'] == 'beef']
-dairy_dataframe = df[df['Trend'] == 'dairy']
+# make trend lower case - mm
+df.columns = ['URL', 'Heading', 'Date', 'trend', 'Text', 'HTML Content', 'compound',
+       'neg', 'neu', 'pos']
+
+
+beef_dataframe = df[df['trend'] == 'cattle'] # was 'beef'
+dairy_dataframe = df[df['trend'] == 'dairy']
 #TODO: Michael
+beef_dataframe.head()
 dairy_dataframe.head()
+
+
+
 
 
 # Hypothesis 1:
 # Null Hypothesis is that sentiment for the beef and the dairy was the same
 
 fig, ax = plt.subplots(ncols=2, figsize=(10,4))
-sns.violinplot(y='compound', x='trend', data=df[tweets.topical_orgs != ''], ax=ax[0])
-sns.boxplot(y='compound', x='trend', data=df[tweets.topical_orgs != ''], ax=ax[1])
+sns.violinplot(y='compound', x='trend', data=beef_dataframe, ax=ax[0]) 
+sns.boxplot(y='compound', x='trend', data=dairy_dataframe, ax=ax[1])
 plt.tight_layout()
-show.plt
+#show.plt  # Was producing error - don't know what it is meant to do.
+
+#plt.show()
 
 
-print('StDev of beef sentiment',np.std(df[df.trend == 'beef']['compound']))
+
+
+
+print('StDev of beef sentiment',np.std(df[df.trend == 'cattle']['compound']))
 print('StDev of NHS sentiment',np.std(df[df.trend == 'dairy']['compound']))
 
 
@@ -118,13 +141,16 @@ print('StDev of NHS sentiment',np.std(df[df.trend == 'dairy']['compound']))
 # 
 # Note: had sample standard deviations not been equal, then set equal_var = False to use Welch's t-statistic (ie. out standard error is calculated differently because we can't pool our two distributions together)
 
-stats.ttest_ind(df[df.trend == 'beef']['compound'],
+
+
+
+stats.ttest_ind(df[df.trend == 'cattle']['compound'],
                 df[df.trend == 'dairy']['compound'], equal_var=True)
 
 
 # Alternatively could have run Statsmodel API's ztest:
 
-sm.stats.ztest(df[df.trend == 'beef']['compound'],
+sm.stats.ztest(df[df.trend == 'cattle']['compound'],
                df[df.trend == 'dairy']['compound'])
 
 
@@ -132,28 +158,28 @@ sm.stats.ztest(df[df.trend == 'beef']['compound'],
 # How small the p-value is, assuming proper statistical process, is how much confidence we have in rejecting the null hypothesis. Because we're saying there's no chance we'd have seen this alternate hypothesis (so far into the tail as it was) if the null was true.
 # We can construct a 95% confidence interval for our difference in sample means to further confirm this:
 
-p_bf = np.mean(df[df.trend == 'beef']['compound'])
+p_bf = np.mean(df[df.trend == 'cattle']['compound'])
 p_dr = np.mean(df[df.trend == 'dairy']['compound'])
 
-num_bf = len(df[df.trend == 'beef'])
+num_bf = len(df[df.trend == 'cattle'])
 num_dr = len(df[df.trend == 'dairy'])
 
 
 # The s.e. for each population (as we're comparing MEANS here) is simply: sigma / sqrt(n)
 
-se_bf = np.std(df[df.trend == 'beef']['compound']) / np.sqrt(num_bf)
+se_bf = np.std(df[df.trend == 'cattle']['compound']) / np.sqrt(num_bf)
 se_dr = np.std(df[df.trend == 'dairy']['compound']) / np.sqrt(num_dr)
 
 
 # Alternatively, statsmodels has a function s.e. of the mean of a distribution:
 
-print('beef sentiment s.e.', stats.sem(df[df.trend == 'beef']['compound'], axis=None))
+print('beef sentiment s.e.', stats.sem(df[df.trend == 'cattle']['compound'], axis=None))
 print('dairy sentiment s.e.', stats.sem(df[df.trend == 'dairy']['compound'], axis=None))
 
 
 # With the standard error for both populations to be used in CI formula below: SE(1,2) = SQR(SE1^2 + SE2^2)
 
-se_diff = np.sqrt(se_br**2 + se_dr**2)
+se_diff = np.sqrt(se_bf**2 + se_dr**2) # Was se_br**2 + se_dr**2
 
 
 diff = p_bf - p_dr
@@ -165,20 +191,23 @@ ucb = diff + (1.96 * se_diff)
 # Visualise sentiment
 # Can we visualise what type of sentiment ifi had for the beef vs the dairy? Let's look at the words that were being used to better understand how the ifi described each entity:
 
-beef_text = " ".join(art for art in df.text[df.trend=='beef'])
-dairy_text = " ".join(art for art in df.text[df.trend=='dairy'])
+
+
+
+beef_text = " ".join(art for art in df.Text[df.trend=='cattle']) # Changed to cattle
+dairy_text = " ".join(art for art in df.Text[df.trend=='dairy'])
 
 stopwords = set(STOPWORDS)
 stopwords.update(['http', 'https', 'www', 'amp', 'ly', 'bit'])
 
-gov_wordcloud = WordCloud(stopwords=stopwords).generate(gov_text)
-nhs_wordcloud = WordCloud(stopwords=stopwords).generate(nhs_text)
+beef_wordcloud = WordCloud(stopwords=stopwords).generate(beef_text)
+dairy_wordcloud = WordCloud(stopwords=stopwords).generate(dairy_text)
 
 fig, ax = plt.subplots(nrows=2, figsize=(10,10))
-ax[0].imshow(gov_wordcloud)
+ax[0].imshow(beef_wordcloud)
 ax[0].set_title('beef')
 ax[0].axis('off')
-ax[1].imshow(nhs_wordcloud)
+ax[1].imshow(dairy_wordcloud)
 ax[1].set_title('dairy')
 ax[1].axis('off')
 plt.tight_layout()
